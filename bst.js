fccfbd2dfde2cba96d0d5599459a4ca47d61711a
Stig Murberg <stig.murberg@gmail.com>
2017-03-04 14:53:07 +0100

    balancing

--- bst.js
+++ bst.js
@@ -6,7 +6,7 @@ export const singleton = head => ({
   head,
   height: 1,
   left: empty(),
-  right: empty(),
+  right: empty()
 })
 
 const tree = (head, left, right) => {
@@ -26,5 +26,30 @@ export const insert = (item, set) => {
   if (item > set.head) {
     return tree(set.head, set.left, insert(item, set.right))
   }
+  return set
+}
+
+export const rotl = set => {
+  return tree(
+    set.right.head,
+    tree(set.head, set.right, set.right.left),
+    set.right.right
+  )
+}
+
+export const rotr = set => {
+  return tree(
+    set.left.head,
+    set.left.left,
+    tree(set.head, set.left.right, set.right)
+  )
+}
+
+export const diff = set => set.right.height - set.left.height
+
+export const balance = set => {
+  const d = diff(set)
+  if (d > 1) return rotl(set)
+  if (d < -1) return rotr(set)
   return set
 }
--- bst.test.js
+++ bst.test.js
@@ -1,4 +1,4 @@
-import { empty, singleton, insert } from './bst'
+import { empty, singleton, insert, rotl, rotr, diff, balance } from './bst'
 
 describe('bst', () => {
 
@@ -30,6 +30,53 @@ describe('bst', () => {
     expect(empty().height).toBe(0)
     expect(singleton(1).height).toBe(1)
     expect(insert(2, singleton(1)).height).toBe(2)
+  })
+
+  it('should rotate left', () => {
+    const set = insert(3, insert(2, singleton(1)))
+    const rot = rotl(set)
+    expect(rot.head).toBe(2)
+    expect(rot.left.head).toBe(1)
+    expect(rot.right.head).toBe(3)
+  })
+
+  it('should rotate right', () => {
+    const set = insert(1, insert(2, singleton(3)))
+    const rot = rotr(set)
+    expect(rot.head).toBe(2)
+    expect(rot.left.head).toBe(1)
+    expect(rot.right.head).toBe(3)
+  })
+
+  it('should calculate the diff of a left leaning tree', () => {
+    const set = insert(1, insert(2, singleton(3)))
+    expect(diff(set)).toBe(-2)
+  })
+
+  it('should calculate the diff of a right leaning tree', () => {
+    const set = insert(3, insert(2, singleton(1)))
+    expect(diff(set)).toBe(2)
+  })
+
+  it('should calculate the diff of a balanced tree', () => {
+    const set = insert(1, insert(3, singleton(2)))
+    expect(diff(set)).toBe(0)
+  })
+
+  it('should balance a right leaning tree', () => {
+    const set = insert(3, insert(2, singleton(1)))
+    const bal = balance(set)
+    expect(bal.head).toBe(2)
+    expect(bal.left.head).toBe(1)
+    expect(bal.right.head).toBe(3)
+  })
+
+  it('should balance a left leaning tree', () => {
+    const set = insert(1, insert(2, singleton(3)))
+    const bal = balance(set)
+    expect(bal.head).toBe(2)
+    expect(bal.left.head).toBe(1)
+    expect(bal.right.head).toBe(3)
   })
 
 })
